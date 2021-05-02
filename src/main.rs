943c0dd67d7e57d7a1550be21d37a495c881b443
droidkid <yuvaraj.chesetti@gmail.com>
2021-05-02 19:09:11 +0530

    Making the first image match the tutorial.

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ fn main() {
     let mut img_buf = image::ImageBuffer::new(img_width, img_height);
 
     for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
-        if y < 100 {
-            continue;
-        } else {
-            let r = ((x as f64) / ((img_width - 1) as f64) * 256.0) as u8;
-            let g = ((y as f64) / ((img_height - 1) as f64) * 256.0) as u8;
-            *pixel = image::Rgb([r, g, 64])
-        }
+        // Red switches on as we move left to right.
+        let r = ((x as f64) / ((img_width - 1) as f64) * 256.0) as u8;
+        // Green switches off as we move top to bottom.
+        let g = (((img_height-y) as f64) / ((img_height - 1) as f64) * 256.0) as u8;
+        // Blue is always fixed.
+        let b = (0.25 * 256.0) as u8;
+        *pixel = image::Rgb([r, g, b])
     }
 
     img_buf.save("gradient.png").unwrap();
